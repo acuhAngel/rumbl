8c3c70a8be78e065f9c8039fac22328fdffa82a7
Angel Acuña <acuhangel@gmail.com>
2023-01-18 21:46:38 -0600

    Changed data source from database

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -2,24 +2,22 @@ defmodule Rumbl.Accounts do
   @moduledoc """
   the accounts context
   """
-
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
   def list_users do
-    [
-      %User{id: "1", name: "jose", username: "josevalim"},
-      %User{id: "2", name: "bruce", username: "redrapids"},
-      %User{id: "3", name: "chris", username: "chrismccord"}
-    ]
+    Repo.all(User)
   end
 
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
   end
 end
